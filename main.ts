69f0984f78dff208bee14d716e9828358734197c
Anton <87050262+Anton-tumurov@users.noreply.github.com>
2023-03-31 20:14:57 +1000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,29 @@
-
+enum RadioMessage {
+    message1 = 49434
+}
+radio.onReceivedNumber(function (receivedNumber) {
+    Othergroupchannel = receivedNumber - 4
+})
+let channel = 0
+let guesschannel2 = 0
+let guesschannel = 0
+let Othergroupchannel = 0
+basic.showString("Ready")
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A) || (input.buttonIsPressed(Button.B) || (input.buttonIsPressed(Button.AB) || input.isGesture(Gesture.Shake)))) {
+        guesschannel = randint(1, 15)
+        if (guesschannel == Othergroupchannel) {
+            guesschannel2 = randint(1, 15)
+            if (guesschannel2 == Othergroupchannel) {
+                channel = guesschannel2
+            } else {
+                channel = guesschannel2
+            }
+        } else {
+            channel = guesschannel
+        }
+        basic.pause(500)
+        radio.sendNumber(channel + 4)
+    }
+    serial.writeNumber(channel)
+})
