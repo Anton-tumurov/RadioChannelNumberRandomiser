0712841edca320a59a9712b9373ab7f8996fd5fb
Anton <87050262+Anton-tumurov@users.noreply.github.com>
2023-03-31 20:26:27 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,13 +2,15 @@ enum RadioMessage {
     message1 = 49434
 }
 radio.onReceivedNumber(function (receivedNumber) {
-    Othergroupchannel = receivedNumber - 4
+    Othergroupchannel = receivedNumber - compilingnumber
 })
 let channel = 0
 let guesschannel2 = 0
 let guesschannel = 0
 let Othergroupchannel = 0
+let compilingnumber = 0
 basic.showString("Ready")
+compilingnumber = randint(0, 10)
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A) || (input.buttonIsPressed(Button.B) || (input.buttonIsPressed(Button.AB) || input.isGesture(Gesture.Shake)))) {
         guesschannel = randint(1, 15)
@@ -23,7 +25,7 @@ basic.forever(function () {
             channel = guesschannel
         }
         basic.pause(500)
-        radio.sendNumber(channel + 4)
+        radio.sendNumber(channel + compilingnumber)
     }
     serial.writeNumber(channel)
     serial.writeNumber(guesschannel)
