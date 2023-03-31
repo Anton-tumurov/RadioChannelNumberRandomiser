f609fbab14509c3a8f73528be40e29acd32afea7
Anton <87050262+Anton-tumurov@users.noreply.github.com>
2023-03-31 21:06:53 +1000

    added load screen

--- main.ts
+++ main.ts
@@ -4,30 +4,98 @@ enum RadioMessage {
 radio.onReceivedNumber(function (receivedNumber) {
     Othergroupchannel = receivedNumber - compilingnumber
 })
+input.onGesture(Gesture.ScreenDown, function () {
+	
+})
 let channel = 0
-let guesschannel2 = 0
-let guesschannel = 0
 let Othergroupchannel = 0
 let compilingnumber = 0
 basic.showString("Ready")
 compilingnumber = randint(0, 10)
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A) || (input.buttonIsPressed(Button.B) || (input.buttonIsPressed(Button.AB) || input.isGesture(Gesture.Shake)))) {
-        guesschannel = randint(1, 15)
-        if (guesschannel == Othergroupchannel) {
-            guesschannel2 = randint(1, 15)
-            if (guesschannel2 == Othergroupchannel) {
-                channel = guesschannel2
-            } else {
-                channel = guesschannel2
+    if (!(input.isGesture(Gesture.ScreenDown))) {
+        if (input.buttonIsPressed(Button.A) || (input.buttonIsPressed(Button.B) || (input.buttonIsPressed(Button.AB) || input.isGesture(Gesture.Shake)))) {
+            let guesschannel2 = 0
+            let guesschannel = 0
+            channel = randint(1, 15)
+            basic.showLeds(`
+                # # # # #
+                . . . . #
+                . . . . #
+                . . . . #
+                . . . . .
+                `)
+            basic.showLeds(`
+                . . # # #
+                . . . . #
+                . . . . #
+                . . . . #
+                . . . # #
+                `)
+            basic.showLeds(`
+                . . . . #
+                . . . . #
+                . . . . #
+                . . . . #
+                . # # # #
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . #
+                # . . . #
+                # # # # #
+                `)
+            basic.showLeds(`
+                . . . . .
+                # . . . .
+                # . . . .
+                # . . . .
+                # # # # #
+                `)
+            basic.showLeds(`
+                # # . . .
+                # . . . .
+                # . . . .
+                # . . . .
+                # # # . .
+                `)
+            basic.showLeds(`
+                # # # # .
+                # . . . .
+                # . . . .
+                # . . . .
+                # . . . .
+                `)
+            basic.showLeds(`
+                # # # # #
+                # . . . #
+                # . . . .
+                . . . . .
+                . . . . .
+                `)
+            basic.showLeds(`
+                # # # # #
+                . . . . #
+                . . . . #
+                . . . . #
+                . . . . .
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . # .
+                # . # . .
+                . # . . .
+                . . . . .
+                `)
+            basic.pause(500)
+            serial.writeNumber(channel)
+            serial.writeNumber(guesschannel)
+            serial.writeNumber(guesschannel2)
+            basic.showNumber(channel)
+            while (true) {
+                basic.pause(100)
             }
-        } else {
-            channel = guesschannel
         }
-        basic.pause(500)
-        radio.sendNumber(channel + compilingnumber)
     }
-    serial.writeNumber(channel)
-    serial.writeNumber(guesschannel)
-    serial.writeNumber(guesschannel2)
 })
